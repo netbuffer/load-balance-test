3bf2a9383bc00f732cdd75457a7798d3392dbbd0
netbuffer <javawiki@163.com>
2018-04-04 12:13:23 +0800

    update

---
+++ README.md
@@ -0,0 +1,4 @@
+# load-balance-test
+
+### step
+* 在终端中执行:`run-app.cmd 9000 9001 9002`来启动多个app实例
--- src/main/java/cn/netbuffer/loadbalancetest/controller/AppController.java
+++ src/main/java/cn/netbuffer/loadbalancetest/controller/AppController.java
@@ -2,25 +2,39 @@ package cn.netbuffer.loadbalancetest.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
-@Controller
-@RequestMapping
 @Slf4j
+@Controller
+@RequestMapping(value = "/")
 public class AppController {
 
     @Resource
     private ApplicationContext applicationContext;
+    @Resource
+    private Environment environment;
 
     @RequestMapping(method = RequestMethod.GET)
-    public String get(Model model) {
+    public String get(ModelMap model) {
         log.info("get method");
-        model.addAttribute("env", applicationContext.getEnvironment());
+        model.addAttribute("env", environment);
         return "app";
+    }
+
+    @RequestMapping(value = "putsession", method = RequestMethod.GET)
+    @ResponseBody
+    public Object putSession(HttpSession session) {
+        if (session.getAttribute("user") == null) {
+            session.setAttribute("user", this + environment.getProperty("server.port"));
+        }
+        return session;
     }
 }
